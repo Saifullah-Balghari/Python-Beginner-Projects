e05719a1c87a2074bc0f716d48b8aad71dfde312
Saifullah-Balghari <balgharisaifullah@gmail.com>
2024-03-10 20:28:20 +0500

    initial commit

---
+++ RamdomPasswordGeneraterApp/main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import random
+import string
+
+
+class PasswordGenerator:
+    def __init__(self) -> None:
+        
+        root = tk.Tk()
+        root.title("Random password generator").title()
+        root.mainloop()
+    
+    def generator(self, uppercase: bool, lowercase: bool, numbers: bool, symbols: bool, length: int) -> None:
+        characters = ""
+        if uppercase:
+            characters += string.ascii_uppercase
+        if lowercase:
+            characters += string.ascii_lowercase
+        if numbers:
+            characters += string.digits
+        if symbols:
+            characters += string.punctuation
+        if not characters:
+            print("You're password should at least contains any one of the above!")
+            return None
+        
+        password = "".join(random.choice(characters) for _ in range(length))
+
+        return password
+        
+
+PasswordGenerator()
