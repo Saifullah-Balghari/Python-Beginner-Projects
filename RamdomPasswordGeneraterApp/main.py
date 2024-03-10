130130c065e726aeee183a981f4795e58470a41e
Saifullah-Balghari <balgharisaifullah@gmail.com>
2024-03-10 23:57:49 +0500

    initial commit

--- RamdomPasswordGeneraterApp/main.py
+++ RamdomPasswordGeneraterApp/main.py
@@ -7,85 +7,67 @@ class RandomPasswordGeneratorApp:
     
     def __init__(self) -> None:
 
-        self.characters = ""
-        self.length = 8
         self.root = tk.Tk()
-
+        self.length = tk.IntVar(value=12)
         self.gui()
-
-        
-
         self.root.mainloop()
     
     def gui(self) -> None:
 
-        self.password_label = tk.Label(self.root, text="")
+        self.root.title("Random password generator").title()
+        self.root.geometry("200x250")
+        
+        self.password_label = tk.Label(self.root, text="", width="20", height="2", relief="sunken")
         self.password_label.pack()
 
-        self.root.title("Random password generator").title()
-        self.root.geometry("300x300")
-
         self.lowerCaseCheckBoxVar = tk.BooleanVar()
-        self.lowerCaseCheckBox = tk.Checkbutton(self.root, text="LowerCase", variable=self.lowerCaseCheckBoxVar, command=self.lowerCase2ru).pack()
-        self.lowerCase = self.lowerCaseCheckBoxVar.get()
+        self.lowerCaseCheckBox = tk.Checkbutton(self.root, text="LowerCase", variable=self.lowerCaseCheckBoxVar)
+        self.lowerCaseCheckBox.pack()
 
         self.upperCaseCheckBoxVar = tk.BooleanVar()
-        self.upperCaseCheckBox = tk.Checkbutton(self.root, text="UpperCase", variable=self.upperCaseCheckBoxVar, command=self.upperCase2ru).pack()
-        self.upperCase = self.upperCaseCheckBoxVar.get()
+        self.upperCaseCheckBox = tk.Checkbutton(self.root, text="UpperCase", variable=self.upperCaseCheckBoxVar)
+        self.upperCaseCheckBox.pack()
 
         self.numbersCheckBoxVar = tk.BooleanVar()
-        self.numbersCheckBox = tk.Checkbutton(self.root, text="Numbers", variable=self.numbersCheckBoxVar, command=self.numbers2ru).pack()
-        self.numbers = self.numbersCheckBoxVar.get()
+        self.numbersCheckBox = tk.Checkbutton(self.root, text="Numbers", variable=self.numbersCheckBoxVar)
+        self.numbersCheckBox.pack()
 
         self.symbolsCheckBoxVar = tk.BooleanVar()
-        self.symbolsCheckBox = tk.Checkbutton(self.root, text="Symbols", variable=self.symbolsCheckBoxVar, command=self.symbols2ru).pack()
-        self.symbols = self.symbolsCheckBoxVar.get()
+        self.symbolsCheckBox = tk.Checkbutton(self.root, text="Symbols", variable=self.symbolsCheckBoxVar)
+        self.symbolsCheckBox.pack()
+
+        self.length_label = tk.Label(self.root, text="Password Length:")
+        self.length_label.pack()
+
+        self.length_entry = tk.Entry(self.root, textvariable=self.length)
+        self.length_entry.pack()
 
         self.generate_button = tk.Button(self.root, text="Generate Password", command=self.generator)
         self.generate_button.pack()
         
     def generator(self) -> None:
-        
-        if self.upperCase == True:
-            self.characters += string.ascii_uppercase
-        if self.lowerCase == True:
-            self.characters += string.ascii_lowercase
-        if self.numbers == True:
-            self.characters += string.digits
-        if self.symbols == True:
-            self.characters += string.punctuation
-        if not self.characters:
-            messagebox.showinfo("Error", "You're password should at least contains any one of the above!")
-        
-        password = "".join(random.choice(self.characters) for _ in range(self.length))
-        self.password_label.config(text=password)
-        
-    def lowerCase2ru(self):
+        self.characters = ""
         if self.lowerCaseCheckBoxVar.get():
-            self.lowerCase = True
-        else:
-            self.lowerCase = False
+            self.characters += string.ascii_lowercase
 
-    def upperCase2ru(self):
         if self.upperCaseCheckBoxVar.get():
-            self.upperCase = True
-        else:
-            self.upperCase = False
+            self.characters += string.ascii_uppercase
 
-    def numbers2ru(self):
         if self.numbersCheckBoxVar.get():
-            self.numbers = True
-        else:
-            self.numbers = False
+            self.characters += string.digits
 
-    def symbols2ru(self):
         if self.symbolsCheckBoxVar.get():
-            self.symbols = True
-        else:
-            self.symbols = False
+            self.characters += string.punctuation
 
+        if not self.characters:
+            messagebox.showinfo("Error", "You must select at least one option!")
+            return None    
+            
+        password_length = self.length.get()
+        password = "".join(random.choice(self.characters) for _ in range(password_length))
+        self.password_label.config(text=password)
 
-def main():        
+def main():
 
     RandomPasswordGeneratorApp()
 
--- RamdomPasswordGeneraterApp/tempCodeRunnerFile.py
+++ RamdomPasswordGeneraterApp/tempCodeRunnerFile.py
@@ -1,17 +1,1 @@
-import tkinter as tk
-from tkinter import messagebox
-
-# Create a Tkinter window
-root = tk.Tk()
-root.title("Tkinter Messagebox Example")
-
-# Function to display the messagebox
-def show_messagebox():
-    messagebox.showinfo("Message", "This is a Tkinter messagebox example.")
-
-# Create a button to trigger the messagebox
-button = tk.Button(root, text="Show Messagebox", command=show_messagebox)
-button.pack(pady=20)
-
-# Run the Tkinter event loop
-root.mainloop()
+        # self.root.geometry("300x300")
